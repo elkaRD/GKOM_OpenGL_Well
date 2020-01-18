2decbc8c8f23f2169ee7053fefead9a489380de4
lwolanin <01133389@pw.edu.pl>
2020-01-18 16:51:32 +0100

    Added Chain link mesh

--- gl_05/ChLinkMesh.cpp
+++ gl_05/ChLinkMesh.cpp
@@ -7,7 +7,7 @@
 #include <glm\gtc\matrix_transform.hpp>
 
 
-ChLinkMesh::ChLinkMesh(): height(5.0f), width(3.0f), radius(0.5f), segments(DEFAULT_SEGMENTS_NUMBER) {}
+ChLinkMesh::ChLinkMesh(): height(0.07f), width(0.04f), radius(0.005f), segments(DEFAULT_SEGMENTS_NUMBER) {}
 
 ChLinkMesh::ChLinkMesh(GLfloat height, GLfloat width, GLfloat radius) : height(height), width(width), 
 	radius(radius), segments(DEFAULT_SEGMENTS_NUMBER) {}
@@ -22,55 +22,45 @@ void ChLinkMesh::initializeMeshVertices(std::vector<Vertex>& vertices, std::vect
 
 void ChLinkMesh::generateVertices(std::vector<Vertex>& vertices)
 {
-	glm::mat4 transform[5] = { 
-		glm::translate(glm::mat4(),glm::vec3((width-radius)/2,(height-width)/2,0.0f)),
-		glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3(0.0f, (height - width) / 2, 0.0f)),glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f)),glm::vec3((width - radius) / 2,(height - width) / 2,0.0f)),
-		glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3(0.0f, (height - width) , 0.0f)),glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)),glm::vec3((width - radius) / 2,(height - width) / 2,0.0f)),
-		glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3(0.0f, (height - width) / 2, 0.0f)),glm::radians(135.0f), glm::vec3(0.0f, 0.0f, 1.0f)),glm::vec3((width - radius) / 2,(height - width) / 2,0.0f)),
-		glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3(0.0f, (height - width) / 2, 0.0f)),glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f)),glm::vec3((width - radius) / 2,(height - width) / 2,0.0f)),
-	//	glm::rotate(glm::translate(glm::mat4(),glm::vec3(0.0f, (height - width) / 2, 0.0f)),glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f))
-	//	
-	//	
-	//
-	//
-		//glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3((width - radius) / 2,0.0f,0.0f)), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)), ),
-		//glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3((width - radius) / 2,0.0f,0.0f)), glm::radians(135.0f), glm::vec3(0.0f, 0.0f, 1.0f)), glm::vec3(0.0f, (height - width) / 2, 0.0f)),
-		//glm::translate(glm::rotate(glm::translate(glm::mat4(),glm::vec3((width - radius) / 2,0.0f,0.0f)), glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f)), glm::vec3(0.0f, (height - width) / 2, 0.0f)),
+	float r = (width - 2 * radius) / 2;
+	glm::mat4 transform[10] = { 
+		glm::translate(glm::mat4(),glm::vec3((width-radius*2)/2,(height-width)/2,0.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(45.0f))*(width-radius*2)/2, (height - width) / 2 + sin(glm::radians(45.0f)) * r, 0.0f)),glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(90.0f)) * (width - radius * 2) / 2, (height - width) / 2 + sin(glm::radians(90.0f)) * r, 0.0f)),glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(135.0f)) * (width - radius * 2) / 2, (height - width) / 2 + sin(glm::radians(135.0f)) * r, 0.0f)),glm::radians(135.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(180.0f)) * (width - radius * 2) / 2, (height - width) / 2 + sin(glm::radians(180.0f)) * r, 0.0f)),glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(-(width - radius * 2) / 2,-(height-width)/2,0.0f)),glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(225.0f)) * (width - radius * 2) / 2, (width - height) / 2 + sin(glm::radians(225.0f)) * r, 0.0f)),glm::radians(225.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(270.0f)) * (width - radius * 2) / 2, (width - height) / 2 + sin(glm::radians(270.0f)) * r, 0.0f)),glm::radians(270.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(315.0f)) * (width - radius * 2) / 2, (width - height) / 2 + sin(glm::radians(315.0f)) * r, 0.0f)),glm::radians(315.0f), glm::vec3(0.0f, 0.0f, 1.0f)),
+		glm::rotate(glm::translate(glm::mat4(),glm::vec3(cos(glm::radians(0.0f)) * (width - radius * 2) / 2, (width - height) / 2 + sin(glm::radians(0.0f)) * r, 0.0f)),glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f))
 	};
-	for (auto i = 0; i < 4; ++i)
+	for (auto i = 0; i < 10; ++i)
 	{
 		for (float angle = 0.0f; angle <= 360.0f; angle += 360.0f / segments)
 		{
 			glm::vec4 radial = glm::vec4(cosf(angle * M_PI / 180.0f) * radius, 0.0f, -sinf(angle * M_PI / 180.0f) * radius, 1.0f);
 				radial = transform[i] * radial;
-			//glm::vec3 aRadial = glm::vec3(cosf(angle * M_PI / 180.0f) * radius, 0.0f, -sinf(angle * M_PI / 180.0f) * radius);
-			//aRadial.y += i*1.0f;
 			glm::vec3 aRadial = glm::vec3(radial.x, radial.y, radial.z);
 			vertices.push_back(Vertex(aRadial, color, glm::vec2(i % 2 == 0 ? 1.0f : 0.0f, 1.0f - angle / 360.0f), glm::normalize(aRadial)));
-			std::cout << aRadial.x << ", " << aRadial.y << ", " << aRadial.z << std::endl;
 		}
 	}
-	std::cout << "Chain Vertices size: " << vertices.size() << std::endl;
+
 }
 
 void ChLinkMesh::generateIndices(std::vector<Vertex>& vertices, std::vector<GLuint>& indices)
 {
-	for (unsigned int l = 0; l < 3; ++l)
+	for (unsigned int l = 0; l < 10; ++l)
 	{
 		unsigned int verticesperlayer = segments + 1;
 		for (unsigned int i = 0; i <= verticesperlayer - 2; ++i)
 		{
-			indices.push_back(i + l * verticesperlayer);
-			indices.push_back(i + 1 + l * verticesperlayer);
-			indices.push_back(i + verticesperlayer * (l + 1));
-			indices.push_back(i + 1 + l * verticesperlayer);
-			indices.push_back(i + verticesperlayer * (l + 1));
-			indices.push_back(i + 1 + verticesperlayer * (l + 1));
+			indices.push_back((i + l * verticesperlayer)%vertices.size());
+			indices.push_back((i + 1 + l * verticesperlayer)% vertices.size());
+			indices.push_back((i + verticesperlayer * (l + 1))%vertices.size());
+			indices.push_back((i + 1 + l * verticesperlayer)%vertices.size());
+			indices.push_back((i + verticesperlayer * (l + 1))%vertices.size());
+			indices.push_back((i + 1 + verticesperlayer * (l + 1))%vertices.size());
 		}
 	}
-
-	for (auto i : indices) {
-		std::cout << i << std::endl;
-	}
-	std::cout << indices.size() << std::endl;
 }
--- gl_05/TestObject.cpp
+++ gl_05/TestObject.cpp
@@ -8,6 +8,12 @@ TestObject::TestObject(GameObject* parent): GameObject(parent)
 void TestObject::start()
 {
 	MeshRenderer* mesh = addMesh(new ChLinkMesh());
+
+	GameObject* backPannel = new GameObject(this);
+	backPannel->setTexture("textures/weiti.png");
+	backPannel->transform.rotate(90.0f, 0.0f, 0.0f);
+	backPannel->transform.translate(0.0f, 0.0f, -1.0f);
+	backPannel->addMesh(new PlaneMesh(3.0f, 5.0f, 1.0f, 1.0f));
 }
 
 void TestObject::update(float delta)
