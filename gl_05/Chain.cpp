a0f45bbe295da617df64b3be4a4e897814967fd2
lwolanin <01133389@pw.edu.pl>
2020-01-19 00:19:59 +0100

    Chain static rolled on roller

--- gl_05/Chain.cpp
+++ gl_05/Chain.cpp
@@ -1,4 +1,7 @@
 #include "Chain.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <glm\detail\func_geometric.hpp>
 
 Chain::Chain(GameObject* parent): GameObject(parent), lenght(100)
 {}
@@ -14,7 +17,7 @@ void Chain::start()
 	//globalParent = new GameObject(upper);
 	std::cout << "Chain starting" << std::endl;
 	this->setTexture("textures/rusty.png");
-	this->transform.translate(-rollerRadius+0.015-0.0025, 0.0f, 0.0f);
+	this->transform.translate(-rollerRadius+0.015-0.0025, 0.0f, 0.015f - 0.0075f);
 	this->transform.rotate(0.0f, 0.0f, 90.0f);
 	GameObject* begin = new GameObject(this);
 	begin->addMesh(new ChLinkMesh(0.03f, 0.02f, 0.0025f));
@@ -26,10 +29,14 @@ void Chain::start()
 		link->addMesh(new ChLinkMesh(0.03f, 0.02f, 0.0025f));
 		link->transform.translate(0.0f, 0.03f - 4 * 0.0025f, 0.0f);
 		if(i==0)
-			link->transform.rotate(0.0f, 90.0f, 90.0f);
+			link->transform.rotate(2.0f, 90.0f, -90.0f);
+		else if(i%2==0)
+		{
+			link->transform.rotate(2.0f, -90.0f, -360.0 * (0.03f - 4* 0.0025f) / (2 * M_PI * (rollerRadius + 0.01f)));//90.0 * (0.03 - 4 * 0.0025)
+		}
 		else
 		{
-			link->transform.rotate(0.0f, 90.0f, 0.0f);//90.0 * (0.03 - 4 * 0.0025)
+			link->transform.rotate(360.0 * (0.03f - 4 * 0.0025f) / (2 * M_PI * (rollerRadius+ 0.01f)), 90.0f, -2.0f);
 		}
 		prev = link;
 	}
